f2442970a8fff1d07f13e96e21e3f4da488b72ca
Jeff Parido <jparido@gmail.com>
2026-02-04 13:56:15 -0500

    ci: add .eslintrc.cjs so GitHub Actions lint step finds config

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  ignorePatterns: ["dist", "*.cjs", "node_modules", "server"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  plugins: ["@typescript-eslint", "react-hooks"],
+  settings: { react: { version: "18.2" } },
+  rules: {
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/no-non-null-assertion": "warn",
+    "no-empty": "warn",
+    "no-case-declarations": "warn",
+    "no-constant-condition": "warn",
+    "@typescript-eslint/no-unused-expressions": "warn",
+    "react-hooks/set-state-in-effect": "warn",
+    "react-hooks/preserve-manual-memoization": "warn",
+  },
+};
